41158c14824540016deec5fabfd7bef2c9f7ef6e
Cloud User <ec2-user@ip-172-31-37-210.us-west-2.compute.internal>
2016-09-13 23:34:58 -0400

    can now successfully execute program and receive database connections from aws rds. can now view server response on browser url.

--- root/android_export.go
+++ root/android_export.go
@@ -1,65 +1,28 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"database/sql"
-	"log"
-	_ "github.com/go-sql-driver/mysql"
+    "fmt"
+    "log"
+    "net/http"
+    "sync"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+var mu sync.Mutex
 
-	db, err := sql.Open("mysql", "doubly_user:db_user1@/Doubly")
-  if err := db.Ping(); err != nil {
-    log.Fatal(err)
-  }
-	println(err)
-
-	io.WriteString(w, GetUsers(w, r))
-/*
-	age := 27
-	rows, err := db.Query("SELECT UserName FROM Users")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var name string
-		if err := rows.Scan(&name); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s is %d\n", name, age)
-		io.WriteString(w, name);
-	}
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
-	}
-*/
+func get_users(w http.ResponseWriter, r *http.Request){
+	mu.Lock()
+	fmt.Fprintf(w, GetUsers(w, r))
+	mu.Unlock()
 }
 
-var mux map[string]func(http.ResponseWriter, *http.Request)
+func get_user_by_id(w http.ResponseWriter, r *http.Request){
+	mu.Lock()
+	fmt.Fprintf(w, GetUserByID(w, r))
+	mu.Unlock()
+}
 
 func main() {
-	server := http.Server{
-		Addr:    ":8000",
-		Handler: &myHandler{},
-	}
-
-	mux = make(map[string]func(http.ResponseWriter, *http.Request))
-	mux["/"] = hello
-
-	server.ListenAndServe()
-}
-
-type myHandler struct{}
-
-func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if h, ok := mux[r.URL.String()]; ok {
-		h(w, r)
-		return
-	}
-
-	io.WriteString(w, "My server: "+r.URL.String())
+    http.HandleFunc("/get_users/", get_users)
+    http.HandleFunc("/get_user_by_id/", get_user_by_id)
+    log.Fatal(http.ListenAndServe(":80", nil))
 }
--- root/users.go
+++ root/users.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"io"
+	//"io"
 	"net/http"
 	"database/sql"
 	"log"
-	"fmt"
+	//"fmt"
 	"strconv"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func GetUsers(w http.ResponseWriter, r *http.Request) string {
-	io.WriteString(w, "Hello world!")
-
-	db, err_open := sql.Open("mysql", "doubly_user:db_user1@/Doubly")
-  if err_open != nil {
+	db, err_open := sql.Open("mysql", "doubly_app:doubly_user1@tcp(doublydb.ct2fpvea2u25.us-west-2.rds.amazonaws.com:3306)/Doubly")
+	if err_open != nil {
 		log.Fatal(err_open)
-  }
-	rows, _ := db.Query("SELECT * FROM Users")
-	defer rows.Close()
-  var users []User
+	}
+	rows, err_query := db.Query("SELECT * FROM Users")
+	if err_query != nil {
+		panic(err_query.Error())
+	}
+	var users []User
 	for rows.Next() {
 		user := User{}
 		var userID, userName, email, password, dob, gender []byte
@@ -30,13 +30,35 @@ func GetUsers(w http.ResponseWriter, r *http.Request) string {
 		user.Password = string(password)
 		user.DOB = string(dob)
 		user.Gender = string(gender)
-		fmt.Printf("%s is %s\n", string(user.UserName), string(user.DOB))
-		//io.WriteString(w, user.UserName);
 		users = append(users, user)
 	}
-	//if err := rows.Err(); err != nil {
-	//	log.Fatal(err)
-	//}
-
+	defer rows.Close()
 	return FormatUsers(users)
+}
+
+func GetUserByID(w http.ResponseWriter, r *http.Request) string {
+	db, err_open := sql.Open("mysql", "doubly_app:doubly_user1@tcp(doublydb.ct2fpvea2u25.us-west-2.rds.amazonaws.com:3306)/Doubly")
+        if err_open != nil {
+                log.Fatal(err_open)
+        }
+        var rID = r.URL.Path[len("/get_user_by_id/"):]
+        rows, err_query := db.Query("SELECT * FROM Users WHERE Users.UserID = " + rID)
+        if err_query != nil {
+                panic(err_query.Error())
+        }
+        var users []User
+        for rows.Next() {
+                user := User{}
+                var userID, userName, email, password, dob, gender []byte
+                rows.Scan(&userID, &userName, &email, &password, &dob, &gender)
+                user.UserID, _ = strconv.Atoi(string(userID))
+                user.UserName = string(userName)
+                user.Email = string(email)
+                user.Password = string(password)
+                user.DOB = string(dob)
+                user.Gender = string(gender)
+                users = append(users, user)
+        }
+        defer rows.Close()
+        return FormatUsers(users)
 }
